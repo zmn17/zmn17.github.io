210efb0ad2b210ff4228abd7f7a1207595b9c6e9
Zamin Ahmadi <asam79961@gmail.com>
2024-05-18 20:58:24 +1000

    fix: video

--- src/components/VideoCam.jsx
+++ src/components/VideoCam.jsx
@@ -1,14 +1,9 @@
 const VideoCam = ({ streamUrl }) => {
   return (
     <div className="video-stream-container">
-      <iframe
-        src={streamUrl}
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        width="100%"
-        height="100%"
-        title="Front Door Camera"
-      ></iframe>
+      <video src={streamUrl} controls autoPlay width="100%" height="500px">
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
